ce7a3444e3020fb6d40299265c30376f97601569
Gustavo R. Ayala <agustavo87@gmail.com>
2024-02-08 17:45:55 -0300

    FEAT: logs requests

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
-const publicPath = '/public'
+const publicPath = 'public'
 const server = http.createServer((req, res) => {
     let filePath = `./${publicPath}` + req.url;
     if (filePath === `./${publicPath}/`) {
@@ -33,13 +33,16 @@ const server = http.createServer((req, res) => {
             if (err.code == 'ENOENT') {
                 res.writeHead(404, { 'Content-Type': 'text/html' });
                 res.end('File not found');
+                console.log(`File not found: ${filePath}`)
             } else {
                 res.writeHead(500);
                 res.end(`Server Error: ${err.code}`);
+                console.log(`Server Error: ${err.code}`)
             }
         } else {
             res.writeHead(200, { 'Content-Type': contentType });
             res.end(content, 'utf-8');
+            console.log(`url: ${req.url} > ${filePath}`)
         }
     });
 });
